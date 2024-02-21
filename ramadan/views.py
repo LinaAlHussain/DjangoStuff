418a5087dc609cc9f157fe6d40fd093cadd4282b
Lina <linaababneh513@gmail.com>
2024-02-22 00:41:53 +0300

    Add some style to the application

--- ramadan/views.py
+++ ramadan/views.py
@@ -1,6 +1,10 @@
+import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Is Ramadan here yet?")
+def index(request) :
+    now = datetime.datetime.now()
+    return render(request, "ramadan/index.html", {
+        "ramadan": now.month == 3 and now.day == 11
+    })
